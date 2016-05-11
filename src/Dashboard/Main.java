57abad7782e767dc7914210c4aced7608591cba9
ydfried <ydfried@gmail.com>
2016-05-11 13:11:53 -0400

    Added title icon

--- src/Dashboard/Main.java
+++ src/Dashboard/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         root.getStylesheets().add("Dashboard/style1.css");
         primaryStage.setTitle("Dashboard");
+        primaryStage.getIcons().add(new Image("file:./src/assets/Dashboard.png"));
         primaryStage.setScene(new Scene(root));
 
         //News.createNewsObject();
